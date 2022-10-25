df51268d8bef8c01adf92520236eab1e6059baaf
ZonianMidian <57513632+ZonianMidian@users.noreply.github.com>
2022-10-25 21:26:24 +0200

    Tiempo [Traducción]

--- pajbot/utils/time_since.py
+++ pajbot/utils/time_since.py
@@ -4,9 +4,9 @@ import math
 def time_since(t1: float, t2: float, time_format: str = "long") -> str:
     time_diff = t1 - t2
     if time_format == "long":
-        num_dict = ["year", "month", "day", "hour", "minute", "second"]
+        num_dict = ["año", "mes", "día", "hora", "minuto", "segundo"]
     else:
-        num_dict = ["y", "M", "d", "h", "m", "s"]
+        num_dict = ["a", "M", "d", "h", "m", "s"]
     num = [
         math.trunc(time_diff / 31536000),
         math.trunc(time_diff / 2628000 % 12),
@@ -22,13 +22,17 @@ def time_since(t1: float, t2: float, time_format: str = "long") -> str:
     while i < 2 and j < 6:
         if num[j] > 0:
             if time_format == "long":
-                time_arr.append(f"{num[j]:g} {num_dict[j]}{'s' if num[j] != 1 else ''}")
+                if num_dict[j] == "mes":
+                    amount = 'es'
+                else:
+                    amount = 's'
+                time_arr.append(f"{num[j]:g} {num_dict[j]}{amount if num[j] != 1 else ''}")
             else:
                 time_arr.append(f"{num[j]}{num_dict[j]}")
             i += 1
         j += 1
 
     if time_format == "long":
-        return " and ".join(time_arr)
+        return " y ".join(time_arr)
 
     return "".join(time_arr)
